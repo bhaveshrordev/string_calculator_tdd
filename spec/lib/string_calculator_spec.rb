83346d8f60ca7e45f9b735801fe806319a68953a
bhaveshrordev <salujabhavesh24@gmail.com>
2025-03-15 19:56:00 +0530

    added more failing test case

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single input" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
   end
 end
