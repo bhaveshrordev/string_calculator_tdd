71ce2ae25120744bf14ee08832424cd78ac12a11
bhaveshrordev <salujabhavesh24@gmail.com>
2025-03-15 20:03:05 +0530

    added test case for handling custom delimiter

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator do
     it "allows new lines as delimiters" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom delimiters" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
